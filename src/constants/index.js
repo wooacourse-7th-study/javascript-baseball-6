95416d390ab9edd6554919e8c7465d698e46a1af
wo-o29 <woogur29@gmail.com>
2024-09-17 21:28:49 +0900

    feat: 상수 추가

---
+++ src/constants/index.js
@@ -0,0 +1,21 @@
+// game
+export const START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+export const INPUT_MESSAGE = "숫자를 입력해주세요 :";
+export const THREE_STRIKE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+export const RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+export const END_MESSAGE = "게임을 종료합니다.";
+
+// validation
+export const OVER_MAX_LENGTH_MESSAGE = "세 자리 이하만 입력 가능합니다.";
+export const ONLY_NUMBER_MESSAGE = "숫자만 입력 가능합니다.";
+export const DUPLICATE_MESSAGE = "중복된 숫자가 있습니다.";
+
+// number
+export const INPUT_VALUE_MAX_LENGTH = 3;
+export const MIN_NUMBER = 1;
+export const MAX_NUMBER = 9;
+
+// count
+export const NOTHING = "낫싱";
+export const STRIKE = "스트라이크";
+export const BALL = "볼";

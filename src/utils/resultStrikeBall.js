1bba7638a916393ea0e8504cb9aed8ec62912adf
DevTeddy123 <ab315601@naver.com>
2024-09-21 21:17:36 +0900

    [feat] 야구게임 결과 출력 함수 구현

---
+++ src/utils/resultStrikeBall.js
@@ -0,0 +1,16 @@
+export const resultStrikeBall = (computerRandom, userInputNumber) => {
+	let strike = 0
+	let ball = 0
+
+	for (let i = 0; i < computerRandom.length; i++) {
+		if (computerRandom[i] === userInputNumber[i]) {
+			strike++
+		} else {
+			if (computerRandom.find(el => el === userInputNumber[i])) {
+				ball++
+			}
+		}
+	}
+
+	return { strike, ball }
+}

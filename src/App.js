3144c369132ccdd0bf9ca7ac31b825bf1822445f
DevTeddy123 <ab315601@naver.com>
2024-09-21 21:19:02 +0900

    [feat] 사용자에게 결과 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
+import { MissionUtils } from '@woowacourse/mission-utils'
+import { randomNum } from './utils/randomNum.js'
+import { userInput } from './utils/userInput.js'
+import { restartGame } from './utils/restartGame.js'
+import { resultStrikeBall } from './utils/resultStrikeBall.js'
+import { message } from './const/message.js'
 class App {
-  async play() {}
+	async play() {
+		//시작
+		MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
+
+		const randomArr = randomNum()
+
+		while (true) {
+			//사용자 입력
+			const userInputNumber = await userInput()
+
+			//컴퓨터 결과 출력
+			const { strike, ball } = resultStrikeBall(randomArr, userInputNumber)
+
+			if (strike === 3) {
+				MissionUtils.Console.print('3' + message.STRIKE)
+				MissionUtils.Console.print(message.END)
+				break
+			}
+
+			if (strike > 0 && ball === 0) {
+				MissionUtils.Console.print(strike + message.STRIKE)
+			}
+			if (ball > 0 && strike === 0) {
+				MissionUtils.Console.print(ball + message.BALL)
+			}
+			if (ball > 0 && strike > 0) {
+				MissionUtils.Console.print(
+					ball + message.BALL + ' ' + strike + message.STRIKE,
+				)
+			}
+			if (ball === 0 && strike === 0) {
+				MissionUtils.Console.print(message.NOTHING)
+			}
+		}
+
+		//재시작 유무
+		await restartGame()
+	}
 }
 
-export default App;
+const app = new App()
+app.play()
+
+export default App

9fc3699b2af35332f61f82897a71db63e9da2d90
wo-o29 <woogur29@gmail.com>
2024-09-24 14:16:08 +0900

    refactor: 상수 객체, 네이밍 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,22 +1,28 @@
 // game
-export const START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-export const INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-export const THREE_STRIKE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-export const RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-export const END_MESSAGE = "게임을 종료합니다.";
+export const MESSAGE = {
+  START_MESSAGE: "숫자 야구 게임을 시작합니다.",
+  INPUT_MESSAGE: "숫자를 입력해주세요 : ",
+  THREE_STRIKE_MESSAGE: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART_MESSAGE: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  END_MESSAGE: "게임을 종료합니다.",
+};
 
 // validation
-export const THREE_LENGTH_MESSAGE = "[ERROR] 세 자리의 숫자만 입력 가능합니다.";
-export const ONLY_NUMBER_MESSAGE = "[ERROR] 숫자만 입력 가능합니다.";
-export const DUPLICATE_MESSAGE = "[ERROR] 중복된 숫자가 있습니다.";
-export const RESTART_INPUT_MESSAGE = "[ERROR] 1 또는 2만 입력 가능합니다.";
+export const VALIDATION = {
+  THREE_LENGTH_MESSAGE: "[ERROR] 세 자리의 숫자만 입력 가능합니다.",
+  ONLY_NUMBER_MESSAGE: "[ERROR] 숫자만 입력 가능합니다.",
+  DUPLICATE_MESSAGE: "[ERROR] 중복된 숫자가 있습니다.",
+  RESTART_INPUT_MESSAGE: "[ERROR] 1 또는 2만 입력 가능합니다.",
+};
+
+// count
+export const COUNT_TEXT = {
+  STRIKE: "스트라이크",
+  BALL: "볼",
+  NOTHING: "낫싱",
+};
 
 // number
 export const INPUT_VALUE_LENGTH = 3;
 export const MIN_NUMBER = 1;
 export const MAX_NUMBER = 9;
-
-// count
-export const NOTHING_TEXT = "낫싱";
-export const STRIKE_TEXT = "스트라이크";
-export const BALL_TEXT = "볼";

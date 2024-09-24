a232c6fbd5c632f9f9d167b1b88ce334db4bf097
wo-o29 <woogur29@gmail.com>
2024-09-24 14:18:04 +0900

    refactor: MESSAGE 프로퍼티 네이밍 수정

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,10 @@
 // game
 export const MESSAGE = {
-  START_MESSAGE: "숫자 야구 게임을 시작합니다.",
-  INPUT_MESSAGE: "숫자를 입력해주세요 : ",
-  THREE_STRIKE_MESSAGE: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  RESTART_MESSAGE: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-  END_MESSAGE: "게임을 종료합니다.",
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  THREE_STRIKE: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  END: "게임을 종료합니다.",
 };
 
 // validation

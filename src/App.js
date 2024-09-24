72dcd10b841eada14a641146a7744c01abbfccb8
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-09-24 11:43:23 +0900

    feat: App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { GAME_MESSAGES } from "../constants/messages.js";
-import randomNumber from "../utils/randomNumber.js";
-import userInputValidation from "../utils/userInputValidation.js";
-import compareToNumber from "../utils/compareToNumber.js";
-import resultPrint from "../utils/resultPrint.js";
+import playGame from "./playGame.js";
+import askForRestart from "../utils/askForRestart.js";
 class App {
   async play() {
+    let continueGame = true;
     MissionUtils.Console.print(GAME_MESSAGES.START);
-    while (true) {
-      //컴퓨터 숫자 생성
-      const comNum = randomNumber();
-
-      while (true) {
-        const userInput = await MissionUtils.Console.readLineAsync(
-          GAME_MESSAGES.INPUT
-        );
-        await userInputValidation(userInput);
-
-        const userNum = Array.from(userInput, Number);
-
-        //숫자 비교후 출력
-        if (resultPrint(compareToNumber(comNum, userNum)) === "finish") {
-          const restart = await MissionUtils.Console.readLineAsync(
-            GAME_MESSAGES.RESTART
-          );
-          if (restart == 1) {
-            break;
-          }
-          if (restart == 2) {
-            MissionUtils.Console.print(GAME_MESSAGES.END);
-            return;
-          }
-        }
-      }
+    while (continueGame) {
+      await playGame();
+      continueGame = await askForRestart();
     }
+    MissionUtils.Console.print(GAME_MESSAGES.END);
   }
 }
 const app = new App();
